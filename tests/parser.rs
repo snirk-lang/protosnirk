4b31134874a5ea9449e36160c0ba2f7d55dbd70c
Snirk Immington <snirk.immington@gmail.com>
2017-03-17 20:11:17 -0400

    Rename test fixtures

--- tests/parser.rs
+++ tests/parser.rs
@@ -1,3 +1,11 @@
 extern crate protosnirk;
 
 use std::io::prelude::*;
+use std::str::Chars;
+
+use protosnirk::lex::{TextIter, Tokenizer, IterTokenizer};
+use protosnirk::parse::{Parser};
+
+fn read_fixture_file(name: &'static str) -> (String, String) {
+    
+}
