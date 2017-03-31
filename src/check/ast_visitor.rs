303a18990c06e895ec9f7dbb7158b956625261f2
Snirk Immington <snirk.immington@gmail.com>
2017-03-31 14:12:20 -0400

    Move checks into separate module

---
+++ src/check/ast_visitor.rs
@@ -0,0 +1,149 @@
+//! Verification struct
+//! Verifies things.
+
+use std::collections::HashMap;
+
+use lex::Token;
+use parse::ast::*;
+use parse::ast::Expression as BaseExpression;
+use super::{ErrorCollector, VerifyError};
+
+/// Trait for expression checkers: visitors on the expression tree.
+pub trait ASTVisitor {
+    fn check_unit(&mut self, unit: &Unit) {
+        for item in unit.get_items() {
+            self.check_item(item);
+        }
+    }
+
+    fn check_expression(&mut self, expr: &BaseExpression) {
+        match *expr {
+            BaseExpression::Assignment(ref assign) => {
+                self.check_assignment(assign)
+            },
+            BaseExpression::Literal(ref lit) => {
+                self.check_literal(lit)
+            },
+            BaseExpression::BinaryOp(ref bin) => {
+                self.check_binary_op(bin)
+            },
+            BaseExpression::Declaration(ref decl) => {
+                self.check_declaration(decl)
+            },
+            BaseExpression::UnaryOp(ref unary_op) => {
+                self.check_unary_op(unary_op)
+            }
+            BaseExpression::VariableRef(ref var_ref) => {
+                self.check_var_ref(var_ref)
+            }
+            BaseExpression::IfExpression(ref if_expr) => {
+                self.check_if_expr(if_expr)
+            }
+            BaseExpression::FnCall(ref fn_call) => {
+                self.check_fn_call(fn_call)
+            }
+        }
+    }
+
+    fn check_statement(&mut self, stmt: &Statement) {
+        match *stmt {
+            Statement::Expression(ref expr) => {
+                self.check_expression(expr)
+            },
+            Statement::Return(ref return_) => {
+                self.check_return(return_)
+            },
+            Statement::DoBlock(ref block) => {
+                self.check_do_block(block)
+            },
+            Statement::IfBlock(ref block) => {
+                self.check_if_block(block)
+            }
+        }
+    }
+
+    fn check_item(&mut self, item: &Item) {
+        match *item {
+            Item::FnDeclaration(ref decl) => {
+                self.check_fn_declaration(decl)
+            }
+        }
+    }
+
+    fn check_block(&mut self, block: &Block) {
+        for stmt in &block.statements {
+            self.check_statement(stmt);
+        }
+    }
+
+    #[inline]
+    fn check_fn_declaration(&mut self, decl: &FnDeclaration) {
+        self.check_block(decl.get_block())
+    }
+
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_do_block(&mut self, block: &DoBlock) {
+        self.check_block(&block.block);
+    }
+
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_assignment(&mut self, assignment: &Assignment) {
+        self.check_var_ref(&assignment.lvalue);
+        self.check_expression(&assignment.rvalue);
+    }
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_literal(&mut self, literal: &Literal) {
+    }
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_binary_op(&mut self, bin_op: &BinaryOperation) {
+        self.check_expression(&bin_op.left);
+        self.check_expression(&bin_op.right);
+    }
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_unary_op(&mut self, unary_op: &UnaryOperation) {
+        self.check_expression(&unary_op.expression);
+    }
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_return(&mut self, ret: &Return) {
+        if let Some(ref val) = ret.value {
+            self.check_expression(val);
+        }
+    }
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_var_ref(&mut self, var_ref: &Identifier) {
+    }
+
+    #[inline]
+    #[allow(unused_variables)]
+    fn check_declaration(&mut self, decl: &Declaration) {
+        self.check_expression(&*decl.value);
+    }
+
+    fn check_if_block(&mut self, if_block: &IfBlock) {
+        for conditional in if_block.get_conditionals() {
+            self.check_expression(&conditional.get_condition());
+            self.check_block(&conditional.get_block());
+        }
+        if let Some(else_info) = if_block.get_else() {
+            self.check_block(&else_info.1);
+        }
+    }
+
+    fn check_if_expr(&mut self, if_expr: &IfExpression) {
+        self.check_expression(if_expr.get_condition());
+        self.check_expression(if_expr.get_true_expr());
+        self.check_expression(if_expr.get_else());
+    }
+
+    fn check_fn_call(&mut self, fn_call: &FnCall) {
+        self.check_var_ref(fn_call.get_name());
+        
+    }
+}
---
+++ src/check/symbol.rs
@@ -0,0 +1,123 @@
+//! Symbol table containing information about a given variable declaration
+
+use std::collections::{HashMap, HashSet};
+use std::ops::{Deref, DerefMut};
+
+use lex::Token;
+use parse::verify::scope::ScopeIndex;
+use parse::ast::{Declaration, Identifier};
+use parse::types::Type;
+
+
+/// Symbol stored in the symbol table
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Symbol {
+    /// Unique index for symbol
+    index: ScopeIndex,
+    /// Cloned token of symbol (i.e. for viewing source)
+    decl_token: Token,
+    /// Whether the symbol has been used
+    used: bool,
+    /// Whether the symbol is mutable (tracked per-reference)
+    mutable: bool,
+    /// Whether the symbol has been mutated
+    mutated: bool,
+    /// The type of the symbol
+    type_: Type,
+    /// The source of the symbol
+    source: Source
+}
+impl Symbol {
+    pub fn new(index: ScopeIndex, token: Token, mutable: bool, type_: Type, source: Source) -> Symbol {
+        Symbol {
+            decl_token: token,
+            index: index,
+            mutable: mutable,
+            used: false,
+            mutated: false,
+            type_: type_,
+            source: source,
+        }
+    }
+    pub fn from_declaration(decl: &Declaration, index: ScopeIndex) -> Symbol {
+        Symbol {
+            decl_token: decl.get_ident().get_token().clone(),
+            index: index,
+            mutable: decl.mutable,
+            used: false,
+            mutated: false,
+            type_: Type::Float,
+            source: Source::Variable,
+        }
+    }
+    pub fn from_parameter(ident: &Identifier, index: ScopeIndex) -> Symbol {
+        Symbol {
+            decl_token: ident.get_token().clone(),
+            index: index,
+            mutable: false, // Just gonna strait up refuse mutable parameters
+            mutated: false,
+            used: false,
+            type_: Type::Float,
+            source: Source::Parameter,
+        }
+    }
+    pub fn from_fn_decl(ident: &Identifier, index: ScopeIndex, type_: Type) -> Symbol {
+        Symbol {
+            decl_token: ident.get_token().clone(),
+            index: index,
+            mutable: false,
+            mutated: false,
+            used: false,
+            type_: type_,
+            source: Source::DeclaredFn,
+        }
+    }
+
+    pub fn get_index(&self) -> &ScopeIndex {
+        &self.index
+    }
+    pub fn get_declaration(&self) -> &Token {
+        &self.decl_token
+    }
+    pub fn is_mutable(&self) -> bool {
+        self.mutable
+    }
+    pub fn is_used(&self) -> bool {
+        self.used
+    }
+    pub fn is_mutated(&self) -> bool {
+        self.mutated
+    }
+    pub fn set_mutated(&mut self) {
+        self.mutated = true;
+    }
+    pub fn set_used(&mut self) {
+        self.used = true;
+    }
+    pub fn get_type(&self) -> &Type {
+        &self.type_
+    }
+    pub fn get_source(&self) -> Source {
+        self.source
+    }
+}
+
+/// Source of a particular symbol
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Source {
+    /// The symbol was declared as a variable
+    Variable,
+    /// The symbol was declared as a fn parameter
+    Parameter,
+    /// The symbol was declared as a function
+    DeclaredFn,
+}
+impl Source {
+    pub fn get_name(self) -> &'static str {
+        match self {
+            Source::Variable => "variable",
+            Source::Parameter => "function parameter",
+            Source::DeclaredFn => "declared function"
+        }
+    }
+}
