db77bb1f2734ffa46b5046446934ef5547a7deb1
Snirk Immington <snirk.immington@gmail.com>
2017-05-03 17:36:56 -0400

    Use cached LLVM when needed

--- build/install-llvm.sh
+++ build/install-llvm.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ -d $HOME/llvm-4.0.0 ]
+    echo "LLVM already installed (cache)";
+    exit;
+fi
+
 # Download llvm.tar.gz from llvm-sys's bitbucket
 wget https://bitbucket.org/tari/llvm-sys.rs/downloads/llvm-4.0.0.linux.tar.xz -O llvm-4.0.0.tar.xz
 # Extract files (no `v` arg to reduce web client spam)
