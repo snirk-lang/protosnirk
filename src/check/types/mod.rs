8920942b06e499b4aaed10f07f08c5ed608e62b4
Snirk Immington <snirk.immington@gmail.com>
2017-05-20 19:28:57 -0400

    Add type checker definitions

--- src/check/types/mod.rs
+++ src/check/types/mod.rs
@@ -1,7 +1,8 @@
 //! Definition of data types in a compiled protosnirk program.
 
-mod types;
 mod inferred;
+mod types;
+mod type_checker;
 
-// Leaving room to expand this module.
-pub use self::types::*;
+pub use self::type_checker::TypeChecker;
+pub use self::types::*; // We're defining parts of the post-AST IR glue here.
