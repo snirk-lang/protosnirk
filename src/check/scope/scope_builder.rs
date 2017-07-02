28e31be1e936123ed49093e8a8d79d45f3d77a7f
Snirk Immington <snirk.immington@gmail.com>
2017-07-02 16:03:21 -0400

    Make scope builder generic

--- src/check/scope/scope_builder.rs
+++ src/check/scope/scope_builder.rs
@@ -1,12 +1,20 @@
+use std::borrow::Borrow;
 use std::collections::HashMap;
+use std::fmt::Debug;
+use std::hash::Hash;
 
 use parse::ScopedId;
 
-/// Generic structure used by the `ScopeChecker` to build the scope mapping.
+/// Generic structure to build a scope mapping.
 ///
 /// # Motivation
-/// Because of block indentation rules, symbol tables must generally be stored in a tree
-/// structure to prevent variables from different blocks from colliding with each other:
+/// Many parts of the AST use `ScopedId` to map symbols to complex data
+/// structures, like mapping between variable names and types, or function
+/// definitions and signatures.
+///
+/// These structures usually have some sort of tree structure based on scope,
+/// where the `ScopedId` must be carefully `.increment()`ed or `.push()`ed.
+/// For example, scoping variables:
 /// ```text
 /// let x = 0
 /// do
@@ -17,23 +25,17 @@ use parse::ScopedId;
 ///     let z = 1
 ///     let y = 0
 /// ```
-/// There are root blocks, child blocks, and sibiling blocks to differentiate.
-///
-/// Because Rust's support for defining complex data structures recursively, and updating them
-/// (especially with forwards and backwards search) is not very great, we chose a simpler apprach:
-/// store the symbols with a special `ScopedId`, a list of indices into the scope tree.
-/// For example, `z` on line 7 has index `[1, 0]` as it's the first declaration in the second
-/// block child of the root.
-///
-///
+/// There are differnt `ScopedId`s being mached to the `x` at different lines.
+/// This analysis is also done for types and there it should mostly be based
+/// on module scoping.
 #[derive(Debug, PartialEq, Clone, Default)]
-pub struct ScopeBuilder {
-    scopes: Vec<HashMap<String, ScopedId>>
+pub struct ScopeBuilder<T: Debug + Hash + Eq> {
+    scopes: Vec<HashMap<T, ScopedId>>
 }
 
-impl ScopeBuilder {
+impl<T> ScopeBuilder<T> {
     /// Create a new empty lexical scope manager
-    pub fn new() -> ScopeBuilder {
+    pub fn new() -> ScopeBuilder<T> {
         ScopeBuilder { scopes: vec![] }
     }
 
@@ -43,55 +45,58 @@ impl ScopeBuilder {
     }
 
     // Pop the topmost scope from the stack
-    pub fn pop(&mut self) -> Option<HashMap<String, ScopedId>> {
+    pub fn pop(&mut self) -> Option<HashMap<T, ScopedId>> {
         self.scopes.pop()
     }
 
     /// Define a new variable in the local scope
-    pub fn define_local(&mut self, name: String, value: ScopedId) {
+    pub fn define_local(&mut self, key: T, value: ScopedId) {
         debug_assert!(!self.scopes.is_empty(),
-            "Attempted to define variable {} with no scopes", name);
+            "Attempted to define {:?} with no scopes", key);
         let last_ix = self.scopes.len() - 1usize;
-        trace!("Defining {} in scope {}", &name, last_ix);
-        &mut self.scopes[last_ix].insert(name, value);
+        trace!("Defining {:?} in scope {}", &key, last_ix);
+        &mut self.scopes[last_ix].insert(key, value);
     }
 
     /// Define a variable in the global scope
-    pub fn define_global(&mut self, name: String, value: ScopedId) {
+    pub fn define_global(&mut self, key: T, value: ScopedId) {
         debug_assert!(!self.scopes.is_empty(),
-            "Attempted to define a global {} with no scopes", name);
-        &mut self.scopes[0].insert(name, value);
+            "Attempted to define a global {:?} with no scopes", key);
+        &mut self.scopes[0].insert(key, value);
     }
 
     /// Get a variable from any scope
-    pub fn get(&self, name: &str) -> Option<&ScopedId> {
-        trace!("Searching for {} in {:#?}", name, self);
+    pub fn get<K: Borrow<T> + Debug>(&self, key: &K) -> Option<&ScopedId> {
+        trace!("Searching for {} in {:#?}", key, self);
         debug_assert!(!self.scopes.is_empty(),
-            "Attempted to search for a variable {} with no scopes", name);
+            "Attempted to search for a variable {:?} with no scopes", key);
         for scope in self.scopes.iter().rev() {
-            trace!("Checking for {} in scope {:?}", name, scope);
-            if let Some(var_ref) = scope.get(name) {
+            trace!("Checking for {:?} in scope {:?}", key, scope);
+            if let Some(var_ref) = scope.get(key) {
                 return Some(var_ref)
             }
         }
-        trace!("Didn't find {}", name);
+        trace!("Didn't find {:?}", key);
         None
     }
 
     /// Get a variable defined in local scopeh
-    pub fn get_local(&self, name: &str) -> Option<&ScopedId> {
+    pub fn get_local<K: Borrow<T> + Debug>(&self, key: &K)
+                                           -> Option<&ScopedId> {
         debug_assert!(!self.scopes.is_empty(),
-            "Attempted to get local var {} with no scopes", name);
+            "Attempted to get local var {:?} with no scopes", key);
         let local_scope_ix = self.scopes.len() - 1usize;
-        self.scopes[local_scope_ix].get(name)
+        self.scopes[local_scope_ix].get(key)
     }
 
     /// Get a variable, starting from the given scope
-    pub fn get_in_scope(&self, name: &str, scope_level: usize) -> Option<&ScopedId> {
+    pub fn get_in_scope<K>(&self, key: &K, scope_level: usize)
+                           -> Option<&ScopedId> where K: Borrow<T> + Debug {
         debug_assert!(self.scopes.len() >= scope_level,
-            "Do not have {} scopes to search, only have {}", scope_level, self.scopes.len());
+            "Do not have {} scopes to search, only have {}",
+            scope_level, self.scopes.len());
         for scope in self.scopes[0..scope_level].iter().rev() {
-            if let Some(var_ref) = scope.get(name) {
+            if let Some(var_ref) = scope.get(key) {
                 return Some(var_ref)
             }
         }
