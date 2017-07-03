b1329c00d63b125741f337d200c8dbe506ca2a4f
Snirk Immington <snirk.immington@gmail.com>
2017-07-03 10:17:41 -0400

    Rewrite and modularize ASTVisitor

--- src/check/visitor/visitor.rs
+++ src/check/visitor/visitor.rs
@@ -48,7 +48,7 @@ pub trait TypeVisitor {
     }
 
     fn visit_named_type_expr(&mut self, named_ty: &NamedTypeExpression);
-    fn visit_block_fn_ty_expr(&mut self, block_fn_ty: &FnTypeExpression);
+    fn visit_fn_type_expr(&mut self, fn_ty: &FnTypeExpression);
     fn visit_inline_fn_ty_expr(&mut self, inline_fn_ty: &InlineFnTypeExpression);
 }
 
@@ -81,6 +81,10 @@ pub trait StatementVisitor {
             }
         }
     }
+    fn visit_return_stmt(&mut self, return_: &Return);
+    fn visit_if_block(&mut self, if_block: &IfBlock);
+    fn visit_do_block(&mut self, do_block: &DoBlock);
+    fn visit_expression(&mut self, expr: &Expression);
 }
 
 /// A visitor which can visit expressions of code.
@@ -104,15 +108,23 @@ pub trait ExpressionVisitor {
             },
             Expression::FnCall(fn_call) => {
                 self.visit_fn_call(fn_call);
+            },
+            Expression::Assignment(assign) => {
+                self.visit_assignment(assign);
+            },
+            Expression::Declaration(declare) => {
+                self.visit_declaration(declare);
             }
         }
     }
-    fn visit_literal_expr(&self, literal: &Literal);
-    fn visit_var_ref(&self, ident: &Identifier);
-    fn visit_if_expr(&self, if_expr: &IfExpression);
-    fn visit_unary_op(&self, unary_op: &UnaryOperation);
-    fn visit_binary_op(&self, bin_op: &BinaryOperation);
-    fn visit_fn_call(&self, fn_call: &FnCall);
+    fn visit_literal_expr(&mut self, literal: &Literal);
+    fn visit_var_ref(&mut self, ident: &Identifier);
+    fn visit_if_expr(&mut self, if_expr: &IfExpression);
+    fn visit_unary_op(&mut self, unary_op: &UnaryOperation);
+    fn visit_binary_op(&mut self, bin_op: &BinaryOperation);
+    fn visit_fn_call(&mut self, fn_call: &FnCall);
+    fn visit_assignment(&mut self, assign: &Assignment);
+    fn visit_declaration(&mut self, declare: &Declaration);
 }
 
 /// A trait which allows a `ItemVisitor` to become a `UnitVisitor`
@@ -125,15 +137,45 @@ pub trait DefaultUnitVisitor : ItemVisitor {
 }
 impl UnitVisitor for DefaultUnitVisitor { }
 
+pub trait DefaultItemVisitor : BlockVisitor { }
+impl ItemVisitor for DefaultItemVisitor {
+    fn visit_inline_fn_decl(&mut self, fn_decl: &mut InlineFnDeclaration) {
+        visit::walk_inline_fn_decl(self, fn_decl);
+    }
+    fn visit_block_fn_decl(&mut self, fn_decl: &mut FnDeclaration) {
+        visit::walk_fn_decl(self, fn_decl);
+    }
+}
+
 /// A trait which allows a `StatementVisitor` to become a
 /// `BlockVisitor` by visiting all statments in a block.
 pub trait DefaultBlockVisitor : StatementVisitor {
     #[inline]
-    fn visit_block(&self, block: &Block) {
+    fn visit_block(&mut self, block: &Block) {
         visit::walk_block(self, block);
     }
 }
 impl BlockVisitor for DefaultBlockVisitor { }
+
+pub trait DefaultStmtVisitor : ExpressionVisitor { }
+impl<V: DefaultStmtVisitor + ExpressionVisitor> StatementVisitor for V {
+    #[inline]
+    fn visit_expression(&mut self, expr: &Expression) {
+        ExpressionVisitor::visit_expression(self, expr);
+    }
+    #[inline]
+    fn visit_do_block(&mut self, do_block: &DoBlock) {
+        visit::walk_do_block(self, do_block);
+    }
+    #[inline]
+    fn visit_if_block(&mut self, if_block: &IfBlock) {
+        visit::walk_if_block(self, if_block);
+    }
+    #[inline]
+    fn visit_return_stmt(&mut self, return_: &Return) {
+        visit::walk_return(self, return_);
+    }
+}
 
 /// Algorithms for visiting children of AST nodes.
 pub mod visit {
@@ -146,6 +188,19 @@ pub mod visit {
         for item in unit.get_items() {
             visitor.visit_item(item);
         }
+    }
+
+    #[inline]
+    pub fn walk_fn_decl<V>(visitor: &mut V, fn_decl: &FnDeclaration)
+                        where V: BlockVisitor {
+        visitor.visit_block(fn_decl.get_block());
+    }
+
+    #[inline]
+    pub fn walk_inline_fn_decl<V>(visitor: &mut V,
+                                  fn_decl: &InlineFnDeclaration)
+                                  where V: BlockVisitor + ExpressionVisitor {
+        visitor.visit_expression(fn_decl.get_expression());
     }
 
     #[inline]
@@ -201,5 +256,23 @@ pub mod visit {
         if let Some((_, block)) = if_block.get_else() {
             visitor.visit_block(block);
         }
+    }
+
+    #[inline]
+    pub fn walk_inline_fn_type<V>(visitor: &mut V,
+                                  fn_type: &InlineFnTypeExpression)
+                                  where V: TypeVisitor {
+        for (_param_name, param_type) in fn_type.get_params() {
+            visitor.visit_type(param_type);
+        }
+    }
+
+    #[inline]
+    pub fn walk_fn_type<V>(visitor: &mut V, fn_type: &FnTypeExpression)
+                           where V: TypeVisitor {
+        for (_param_name, param_type) in fn_type.get_params() {
+            visitor.visit_type(param_type);
+        }
+        visitor.visit_type(fn_type.get_return_type());
     }
 }
