7c7f3182e1780b9a50b2399d84b044c324674ea8
Snirk Immington <snirk.immington@gmail.com>
2017-07-10 23:56:37 -0400

    First iteration of item type checking

--- src/check/types/mod.rs
+++ src/check/types/mod.rs
@@ -1,8 +1,12 @@
 //! Definition of data types in a compiled protosnirk program.
 
-mod inferred;
 mod types;
+mod environment;
+
+mod expression_collector;
+mod item_collector;
 mod type_checker;
 
 pub use self::type_checker::TypeChecker;
+pub use self::environment::*;
 pub use self::types::*; // We're defining parts of the post-AST IR glue here.
