a52080ac2778bd9176c0f7d4401293601604d9d5
Snirk Immington <snirk.immington@gmail.com>
2017-08-29 17:20:30 -0400

    Name fn parameters during item check

--- src/check/scope/names/item_namer.rs
+++ src/check/scope/names/item_namer.rs
@@ -34,37 +34,51 @@ impl<'err, 'builder> DefaultUnitVisitor
     for ItemVarIdentifier<'err, 'builder> { }
 
 impl<'err, 'builder> ItemVisitor for ItemVarIdentifier<'err, 'builder> {
-    fn visit_inline_fn_decl(&mut self, inline_fn: &InlineFnDeclaration) {
-        debug_assert!(inline_fn.get_ident().get_id().is_default(),
-            "Inline fn {:?} already had an ID", inline_fn);
-        if let Some(previous_def) = self.builder.get(inline_fn.get_name()) {
-            // fn has been previously defined
-        }
-        else {
-            self.current_id.increment();
-            let fn_id = self.current_id.clone();
-            trace!("Created id {:?} for inline fn {}",
-                fn_id, inline_fn.get_name());
-            self.builder.define_local(inline_fn.get_name().to_string(),
-                                      fn_id.clone());
-            inline_fn.get_ident().set_id(fn_id);
-        }
-    }
-
     fn visit_block_fn_decl(&mut self, block_fn: &BlockFnDeclaration) {
         debug_assert!(block_fn.get_ident().get_id().is_default(),
             "Block fn {:?} already had an ID", block_fn);
         if let Some(previous_def) = self.builder.get(block_fn.get_name()) {
             // fn has been previously defined
+            let error_text = format!("Function {} is already declared",
+                block_fn.get_name());
+            self.errors.add_error(CheckerError::new(
+                block_fn.get_token().clone(), vec![], error_text
+            ));
+            return
         }
-        else {
-            self.current_id.increment();
-            let fn_id = self.current_id.clone();
-            trace!("Created id {:?} for block fn {}",
-                fn_id, block_fn.get_name());
-            self.bilder.define_local(block_fn.get_name().to_string(),
-                                     fn_id.clone());
-            block_fn.get_ident().set_id(fn_id);
+        self.current_id.increment();
+        let fn_id = self.current_id.clone();
+        trace!("Created id {:?} for block fn {}",
+            fn_id, block_fn.get_name());
+        self.builder.define_local(block_fn.get_name().to_string(),
+                                 fn_id.clone());
+        block_fn.get_ident().set_id(fn_id);
+
+        // Also name the params here
+        let mut param_id = self.current_id.pushed();
+        //self.builder.new_scope()
+        for (param, _param_type) in block_fn.get_params() {
+            // Identify params internally with {fn_name}:{param_name}.
+            let mut param_name = String::new();
+            param_name.push(block_fn.get_name());
+            param_name.push(":");
+            param_name.push(param.get_text());
+
+            if let Some(previous_def) = self.builder.get(&param_name) {
+                let error_text = format!(
+                    "Parameter {} of function {} is already declared",
+                    param.get_name(), block_fn.get_name());
+                self.errors.add_error(CheckerError::new(
+                    block_fn.get_token().clone(), vec![], error_text
+                ));
+                return // Stop checking params if there's a dupe.
+            }
+
+            trace!("Created id {:?} for {} param {}",
+                param_id, block_fn.get_name(), param.get_text());
+            param_id.increment();
+            self.builder.define_local(param_name, param_id.clone());
+            param.set_id(param_id.clone());
         }
     }
 }
