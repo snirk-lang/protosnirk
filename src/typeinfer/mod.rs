882ec8368dc1c1a705b4afb165a5e741ec00b544
Snirk Immington <snirk.immington@gmail.com>
2017-10-11 12:28:23 -0400

    Move TypeId collecting visitors to submodule
    Rename and improve visitors which collect TypeIds
    for type inference using the TypeEquationBuilder.

--- src/typeinfer/mod.rs
+++ src/typeinfer/mod.rs
@@ -1,26 +1,14 @@
 //! Definition of data types in a compiled protosnirk program.
 
-mod types;
-mod environment;
+mod identify;
 
-mod expression_collector;
-mod item_collector;
+mod concrete_type;
+mod inferred_type;
+mod inference_source;
 mod type_checker;
 
-pub use self::type_checker::TypeChecker;
-pub use self::environment::*;
-pub use self::types::*; // We're defining parts of the post-AST IR glue here.
+use self::inferred_type::{InferredType, TypeEquation};
 
-use parse::ScopedId;
-
-thread_local! {
-    static TYPE_IDENT_INT: ScopedId
-        = ScopedId::default().incremented();
-    static TYPE_IDENT_BOOL: ScopedId
-        = ScopedId::default().incremented().incremented();
-
-    static TYPE_ID_INT: TypeId
-        = TypeId::default().next();
-    static TYPE_ID_BOOL: TypeId
-        = TypeId::default().next().next();
-}
+pub use self::inference_source::InferenceSource;
+pub use self::concrete_type::*;
+pub use self::identify::ASTTypeIdentifier;
