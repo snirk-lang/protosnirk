ed60bc8620fa0a7e69ad526d9e160df235eb23f6
Snirk Immington <snirk.immington@gmail.com>
2018-05-14 12:43:02 -0400

    Add specalized methods on llvm::Value

--- src/llvm/module.rs
+++ src/llvm/module.rs
@@ -1,12 +1,13 @@
 //! Bindings to LLVM module objects
 
-use std::rc::Rc;
+use std::ffi::{CStr, CString};
 use libc::{c_char};
 
 use llvm_sys::core::*;
 use llvm_sys::prelude::*;
+use llvm_sys::analysis::{LLVMVerifierFailureAction, LLVMVerifyModule};
 
-use llvm::context::Context;
+use llvm::{Context, Type, Value};
 
 llvm_wrapped! {
     pub struct Module {
@@ -18,9 +19,9 @@ llvm_wrapped! {
 impl Module {
     /// Create a new Module with the given name
     pub fn with_name(name: &str) -> Module {
-        let c_name = name.as_ptr() as *const c_char;
+        let c_name = CString::new(name).unwrap();
         let module_ref = unsafe {
-            LLVMModuleCreateWithName(c_name)
+            LLVMModuleCreateWithName(c_name.as_ptr() as *const c_char)
         };
         Module::from_ref(module_ref)
     }
@@ -41,5 +42,37 @@ impl Module {
         Context::from_ref(context)
     }
 
+    pub fn get_type_by_name(&self, name: &str) -> Option<Type> {
+        let c_name = CString::new(name).unwrap();
+        let ty_ref = unsafe {
+            LLVMGetTypeByName(**self, c_name.as_ptr() as *const c_char)
+        };
+        if ty_ref.is_null() {
+            None
+        }
+        else {
+            Some(Type::from_ref(ty_ref))
+        }
+    }
 
+    pub fn add_function(&self, name: &str, ty: Type) -> Value {
+        let c_name = CString::new(name).unwrap();
+        Value::from_ref(unsafe {
+            LLVMAddFunction(**self, c_name.as_ptr() as *const c_char, *ty)
+        })
+    }
+
+    pub fn verify(&self, action: LLVMVerifierFailureAction) -> Result<(), String> {
+        let mut error = 0 as *mut c_char;
+        unsafe {
+            if LLVMVerifyModule(**self, action, &mut error) > 0 {
+                let cstr_buf = CStr::from_ptr(error);
+                let result = String::from_utf8_lossy(cstr_buf.to_bytes()).into_owned();
+                LLVMDisposeMessage(error);
+                Err(result)
+            } else {
+                Ok(())
+            }
+        }
+    }
 }
--- src/llvm/value.rs
+++ src/llvm/value.rs
@@ -1,11 +1,80 @@
 //! Bindings to LLVM value objects
 
+use std::ffi::CString;
+use libc::{c_char, c_uint};
+
 use llvm_sys::core::*;
 use llvm_sys::prelude::*;
+use llvm_sys::analysis::{LLVMVerifierFailureAction, LLVMVerifyFunction};
+
+use llvm::BasicBlock;
 
 llvm_wrapped! {
     pub struct Value {
         value: LLVMValueRef,
         dispose: drop // Value is entirely owned by Context
     }
+}
+
+impl Value {
+    // general methods
+
+    pub fn is_null_value(&self) -> bool {
+        unsafe {
+            LLVMIsNull(**self) == 0
+        }
+    }
+
+    // methods on FunctionValue
+
+    pub fn count_params(&self) -> usize {
+        unsafe {
+            LLVMCountParams(**self) as usize
+        }
+    }
+
+    pub fn get_params(&self) -> Vec<Value> {
+        let param_count = self.count_params();
+        if param_count == 0 {
+            return vec![]
+        }
+        let mut params = Vec::with_capacity(param_count);
+        unsafe {
+            LLVMGetParams(**self, params.as_mut_ptr());
+        }
+        // Could be done with a transmute, but this way we get null checks too.
+        params.iter().map(|value| Value::from_ref(*value)).collect()
+    }
+
+    pub fn set_name(&self, name: &str) {
+        let c_name = CString::new(name).unwrap();
+        unsafe {
+            LLVMSetValueName(**self, c_name.as_ptr() as *const c_char);
+        }
+    }
+
+    pub fn verify(&self, action: LLVMVerifierFailureAction) -> bool {
+        unsafe {
+            LLVMVerifyFunction(**self, action) == 0
+        }
+    }
+
+    // methods on PhiNode
+
+    pub fn add_incoming<V, B>(&self, values: V, blocks: B)
+    where V: IntoIterator<Item=Value>, B: IntoIterator<Item=BasicBlock> {
+        use std::ops::DerefMut;
+        let in_vals: Vec<_> = values.into_iter().map(|val| *val.deref_mut()).collect();
+        let in_blocks: Vec<_> = blocks.into_iter().map(|block| *block.deref_mut()).collect();
+
+        debug_assert_eq!(in_vals.len(), in_blocks.len());
+
+        unsafe {
+            LLVMAddIncoming(**self,
+                            in_vals.as_mut_ptr(),
+                            in_blocks.as_mut_ptr(),
+                            in_vals.len() as c_uint)
+        }
+    }
+
 }
