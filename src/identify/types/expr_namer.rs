b291c530e8399a720d82b5c7a38fb76fb160dc5d
Snirk Immington <snirk.immington@gmail.com>
2018-05-21 18:42:30 -0400

    Add ExprTypeIdentifier
    Identifies type expressions within function code
    (such as explicit let)

---
+++ src/identify/types/expr_namer.rs
@@ -0,0 +1,86 @@
+//! AST visitor which assigns the ScopedIds of type expressions
+//! within items.
+
+use ast::*;
+use check::{CheckerError, ErrorCollector};
+use identify::TypeScopeBuilder;
+use identify::types::TypeIdentifier;
+use visit;
+use visit::visitor::*;
+
+/// Identifies the names of type expressions within functions, namely
+/// cases where named types are explicitly declared.
+pub struct ExprTypeIdentifier<'err, 'builder> {
+    errors: &'err mut ErrorCollector,
+    builder: &'builder mut TypeScopeBuilder,
+    current_id: ScopedId
+}
+
+impl<'err, 'builder> ExprTypeIdentifier<'err, 'builder> {
+    pub fn new(errors: &'err mut ErrorCollector,
+               builder: &'builder mut TypeScopeBuilder)
+               -> ExprTypeIdentifier<'err, 'builder> {
+        ExprTypeIdentifier { errors, builder, current_id: ScopedId::default() }
+    }
+}
+
+impl<'err, 'builder> DefaultUnitVisitor
+    for ExprTypeIdentifier<'err, 'builder> { }
+
+impl<'err, 'builder> ItemVisitor for ExprTypeIdentifier<'err, 'builder> {
+    fn visit_block_fn_decl(&mut self, block_fn: &BlockFnDeclaration) {
+        if block_fn.get_id().is_default() {
+            trace!("Skipping unidentified block fn {}", block_fn.get_name());
+            return
+        }
+        visit::walk_block(self, block_fn.get_block());
+    }
+}
+
+impl<'err, 'builder> DefaultBlockVisitor
+    for ExprTypeIdentifier<'err, 'builder> { }
+
+// The goal of being an ExpressionVisitor is to find places where types are
+// explicitly used within a function. For now this occurs only within a
+// declaration, which is an expression:
+// https://github.com/immington-industries/protosnirk/issues/30
+
+impl<'err, 'builder> DefaultStmtVisitor
+    for ExprTypeIdentifier<'err, 'builder> { }
+
+impl<'err, 'builder> ExpressionVisitor
+    for ExprTypeIdentifier<'err, 'builder> {
+
+    fn visit_literal_expr(&self, _literal: &Literal) { }
+
+    fn visit_var_ref(&self, _ident: &Identifier) { }
+
+    fn visit_if_expr(&self, if_expr: &IfExpression) {
+        visit::walk_if_expr(self, if_expr);
+    }
+
+    fn visit_unary_op(&self, unary_op: &UnaryOperation) {
+        visit::walk_unary_op(self, unary_op);
+    }
+
+    fn visit_binary_op(&self, binary_op: &BinaryOperation) {
+        visit::walk_bin_op(self, binary_op);
+    }
+
+    fn visit_fn_call(&self, fn_call: &FnCall) {
+        // Technically shouldn't be allowed, but see #30 above.
+        for arg in fn_call.get_args() {
+            self.visit_expression(arg);
+        }
+    }
+
+    fn visit_assignment(&self, assign: &Assignment) { }
+
+    fn visit_declaration(&self, declaration: &Declaration) {
+        if let Some(ref decl_ty) = declaration.get_type_decl() {
+            // visit the declaration
+            TypeIdentifier::new(self.errors, self.builder)
+                          .visit(decl_ty);
+        }
+    }
+}
