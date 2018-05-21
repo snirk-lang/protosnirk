69fb81daaeb58241bb5dc6213ed6645596a9b28f
Snirk Immington <snirk.immington@gmail.com>
2018-05-21 18:41:26 -0400

    Add TypeIdentifier visitor for type expressions

--- src/identify/types/mod.rs
+++ src/identify/types/mod.rs
@@ -1,3 +1,9 @@
+mod type_identifier;
+use self::type_identifier::TypeIdentifier;
+
 mod item_namer;
+mod expr_namer;
+
 
 pub use self::item_namer::ItemTypeIdentifier;
+pub use self::expr_namer::ExprTypeIdentifier;
---
+++ src/identify/types/type_identifier.rs
@@ -0,0 +1,44 @@
+//! Visitor which walks through a TypeExpression to assign its
+//! `ScopedId`.
+
+use ast::ScopedId;
+use ast::types::*;
+use check::{CheckerError, ErrorCollector};
+use identify::TypeScopeBuilder;
+use visit;
+use visit::visitor::*;
+
+/// Visitor which identifies TypeExpressions,
+/// by assigning their IDs to those found in
+/// a `TypeScopeBuilder`.
+#[derive(Debug)]
+pub struct TypeIdentifier<'err, 'builder> {
+    errors: &'err mut ErrorCollector,
+    /// New types cannot be defined yet.
+    builder: &'builder TypeScopeBuilder,
+}
+
+impl<'err, 'builder> TypeIdentifier<'err, 'builder> {
+    pub fn new(errors: &'err mut ErrorCollector,
+               builder: &'builder TypeScopeBuilder)
+               -> TypeIdentifier<'err, 'builder> {
+        TypeIdentifier { errors, builder }
+    }
+}
+
+impl<'err, 'builder> TypeVisitor for TypeIdentifier<'err, 'builder> {
+    fn visit_named_type_expr(&mut self, named_ty: &NamedTypeExpression) {
+        if let Some(ref type_id) =
+            self.builder.get_named_type_id(named_ty.get_name()) {
+            // Found the already defined type.
+            named_ty.set_id(type_id.clone());
+        }
+        else {
+            self.errors.add_error(CheckerError::new(
+                named_ty.get_ident().get_token().clone(),
+                vec![],
+                format!("Unknown type {}", named_ty.get_name())
+            ));
+        }
+    }
+}
