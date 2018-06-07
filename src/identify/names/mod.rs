295755202d996a63ec0bd948826a0f70d6e31b4e
Snirk Immington <snirk.immington@gmail.com>
2018-06-07 17:09:06 -0400

    Add OriginManager to track lvalues
    Convenient builder to simplify code in identify/
    which tracks which blocks return values

--- src/identify/names/mod.rs
+++ src/identify/names/mod.rs
@@ -1,5 +1,7 @@
 mod expr_namer;
 mod item_namer;
+mod origin_manager;
+use self::origin_manager::OriginManager;
 
 pub use self::item_namer::ItemVarIdentifier;
 pub use self::expr_namer::ExpressionVarIdentifier;
