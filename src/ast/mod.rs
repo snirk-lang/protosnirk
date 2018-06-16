2470122ad41385f43ee7c486f60c73f3a71e6f7f
Snirk Immington <snirk.immington@gmail.com>
2018-06-16 17:23:21 -0400

    Make ast structures have private fields

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -31,7 +31,7 @@ use lex::Token;
 /// Basic identifier type
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Identifier {
-    pub token: Token,
+    token: Token,
     id: RefCell<ScopedId>,
 }
 impl Identifier {
@@ -39,7 +39,7 @@ impl Identifier {
         Identifier { token, id: RefCell::default() }
     }
     pub fn get_name(&self) -> &str {
-        &self.token.text
+        &self.token.get_text()
     }
     pub fn get_token(&self) -> &Token {
         &self.token
