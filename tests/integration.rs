f7454eeb2acdbc128477d8b93eb1343048184860
Snirk Immington <snirk.immington@gmail.com>
2018-07-01 08:20:20 -0600

    Remove manual integration test runner

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,9 +1,6 @@
 //! Test runner for protosnirk tests
 
-extern crate colored;
-extern crate num_cpus;
 extern crate protosnirk;
-extern crate workerpool;
 
 #[macro_use]
 extern crate derive_integration_tests;
@@ -14,117 +11,8 @@ use std::io::{self, Read, Write};
 use std::sync::mpsc::channel;
 use std::path::{Path, PathBuf};
 
-use colored::Colorize;
-use workerpool::{Pool, thunk::{Thunk, ThunkWorker}};
-
 use protosnirk::llvm::{Context};
 use protosnirk::pipeline::{Runner, CompileRunner};
-
-/// Represents an object which can run Snirk tests.
-#[derive(Debug)]
-pub struct Tester {
-    root_path: PathBuf
-}
-
-impl Tester {
-    pub fn new<P: AsRef<Path>>(folder: P) -> Tester {
-        // Cargo places test files in a path like
-        // target/debug/deps/file-{hex-mangling?}
-        //    .. /  .. / .. /tests
-        writeln!(io::stderr(), "\n     {} tests/{}\n",
-            "Integration tests".green().bold(), folder.as_ref().display());
-
-        let arg = env::args().next().expect("$0");
-        let source_path = Path::new(&arg);
-        let full_path = source_path
-            .parent().expect("Bad test dir") // deps
-            .parent().expect("Bad test dir") // debug/release
-            .parent().expect("Bad test dir") // target
-            .parent().expect("Bad test dir")
-            .join("tests")
-            .join(folder);
-
-        Tester { root_path: full_path }
-    }
-
-    pub fn create_tests(&self) -> io::Result<Vec<Test>> {
-        let mut tests = Vec::new();
-        let mut dirs = Vec::new();
-        dirs.push(self.root_path.clone());
-        while let Some(dir) = dirs.pop() {
-            for entry_result in try!(fs::read_dir(dir)) {
-                let entry = try!(entry_result);
-                if entry.path().is_dir() {
-                    dirs.push(entry.path());
-                }
-                else {
-                    let mut buffer = String::new();
-                    let mut file = try!(File::open(entry.path()));
-                    try!(file.read_to_string(&mut buffer));
-                    //tests.push(Test::new(entry.path().as_path(), buffer));
-                }
-            }
-        }
-        Ok(tests)
-    }
-
-    pub fn test_all(
-            &self,
-            tests: Vec<Test>,
-            runner: &'static (Fn(Test) -> TestResult + Send + Sync))
-            -> Result<(), usize> {
-        let name = self.root_path.file_stem()
-            .expect("Checked expect")
-            .to_string_lossy();
-
-        let total_tests = tests.len();
-
-        writeln!(io::stderr(), "found {} test files\n", total_tests);
-
-        let pool = Pool::<ThunkWorker<()>>::with_name(name.into(),
-            num_cpus::get());
-
-        let (tx, rx) = channel();
-        for test in tests {
-            let thunk_tx = tx.clone();
-            pool.execute(Thunk::of(move || {
-                let name = test.name().to_string();
-                let result = runner(test);
-                thunk_tx.send((name, result)).unwrap();
-            }));
-        }
-        drop(tx);
-
-        let mut pass_count: usize = 0;
-
-        while let Ok((tested, result)) = rx.recv() {
-            if result.is_ok() {
-                pass_count += 1;
-                writeln!(io::stderr(), "file {} ... {}",
-                    tested, "ok".green());
-            }
-            else {
-                writeln!(io::stderr(), "file {} ... {}",
-                    tested, "fail".red());
-
-                writeln!(io::stderr(), "> {}",
-                    result.expect_err("Checked expect"));
-            }
-
-        }
-
-        writeln!(io::stderr(),
-            "\nintegration tests: ok. {} passed; {} failed; 0 ignored;",
-            pass_count, total_tests - pass_count);
-
-        if total_tests - pass_count > 0 {
-            Err(total_tests - pass_count)
-        }
-        else {
-            Ok(())
-        }
-    }
-}
 
 /// Flags for which parts of the compile pipeline tests must go through.
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -257,36 +145,6 @@ fn compile_runner(test: Test) -> TestResult {
         let _module = compiler.compile(checked, false);
     }
     Ok(())
-}
-
-fn lint_runner(test: Test) -> TestResult {
-    Ok(())
-}
-
-const INTEGRATION_TEST_DIRS: &[(&str, fn(Test) -> TestResult)] = &[
-    ("compile", compile_runner),
-    //("lint", lint_runner),
-    //("run", lint_runner)
-];
-
-fn integration_tests() {
-    let mut fail_count = 0;
-    for (stage, runner) in INTEGRATION_TEST_DIRS {
-        let tester = Tester::new(stage);
-        let files = tester.create_tests()
-            .expect("Unable to find test files");
-        match tester.test_all(files, runner) {
-            Ok(()) => {},
-            Err(failed) => {
-                fail_count += failed;
-            }
-        }
-    }
-    write!(io::stderr(), "\n").ok();
-
-    if fail_count > 0 {
-        panic!("{} total integration tests failed", fail_count);
-    }
 }
 
 #[derive(IntegrationTests)]
