5f79b661c79d7209017a9a6639c89416837a1b36
S. B. Immington <snirk.immington@gmail.com>
2019-01-15 13:32:31 -0500

    Finish removing parser maps

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -4,8 +4,7 @@
 //! source tree.
 
 use std::borrow::Cow;
-use std::collections::{HashMap, VecDeque};
-use std::rc::Rc;
+use std::collections::VecDeque;
 
 use lex::{CowStr, Token, TokenType, Tokenizer};
 use parse::ParseError;
@@ -18,14 +17,6 @@ pub struct Parser<T: Tokenizer> {
     tokenizer: T,
     /// Lookahead queue for peeking
     lookahead: VecDeque<Token>,
-    /// Parsers used for prefix symbols in statements (`return`, `do`)
-    stmt_prefix_parsers: HashMap<TokenType, Rc<PrefixParser<Statement, T> + 'static>>,
-    /// Parsers used for infix symbols in expressions (`+`, `<`)
-    expr_infix_parsers: HashMap<TokenType, Rc<InfixParser<Expression, T> + 'static>>,
-    /// Parsers used for prefix symbols in expressions (`not`, `let`)
-    expr_prefix_parsers: HashMap<TokenType, Rc<PrefixParser<Expression, T> + 'static>>,
-    /// Parses for parsing program items (struct/enum/fn declarations, etc.)
-    item_parsers: HashMap<TokenType, Rc<PrefixParser<Item, T> + 'static>>,
     /// Allows the parser to skip over unneeded indentation
     indent_rules: Vec<IndentationRule>,
 }
@@ -231,36 +222,22 @@ impl<T: Tokenizer> Parser<T> {
         trace!("Parsing expression(precedence={:?}) with {}", precedence, token);
         if _indented { trace!("Parsing indented expression"); }
         let token_type = token.get_type();
+        use self::TokenType::*;
         let mut left = try!(match token_type {
-            TokenType::EOF => {
-                trace!("Unexpected EOF parsing expression");
-                Err(ParseError::EOF)
-            },
-            TokenType::EndBlock => {
-                trace!("Unexpected EndBlock parsing expression");
-                Err(ParseError::EOF)
-            },
-            TokenType::If => {
-                trace!("Parsing an if expression");
-                IfExpressionParser { }.parse(self, token)
-            },
-            TokenType::Minus =>{
-                trace!("Parsing a subtraction expression");
-                UnaryOpExprSymbol::with_precedence(Precedence::NumericPrefix)
-                    .parse(self, token)
-            },
-            TokenType::LeftParen => {
-                trace!("Parsing a paren expression");
-                ParensParser { }.parse(self, token)
-            },
-            TokenType::Ident => {
-                trace!("Parsing an identifier reference");
-                IdentifierParser { }.parse(self, token)
-            },
-            TokenType::Literal => {
-                trace!("Parsing a literal expression");
-                LiteralParser { }.parse(self, token)
-            },
+            EOF => Err(ParseError::EOF),
+
+            EndBlock => Err(ParseError::EOF),
+
+            If => IfExpressionParser { }.parse(self, token),
+
+            Minus | Plus => UnaryOpExprSymbol { }.parse(self, token),
+
+            LeftParen => ParensParser { }.parse(self, token),
+
+            Ident => IdentifierParser { }.parse(self, token),
+
+            Literal => LiteralParser { }.parse(self, token),
+
             _ => {
                 trace!("Could not find parser");
                 return Err(ParseError::LazyString(format!("Unexpected token {:?}", token)))
@@ -275,77 +252,25 @@ impl<T: Tokenizer> Parser<T> {
             token = new_token;
             let token_type = token.get_type();
             left = try!(match token_type {
-                TokenType::Equals => {
-                    trace!("Parsing an assignment");
-                    AssignmentParser { }.parse(self, left, token)
-                },
-                TokenType::Plus => {
-                    trace!("Parsing infix addition");
-                    BinOpExprSymbol::with_precedence(Precedence::AddSub)
-                        .parse(self, left, token)
-                },
-                TokenType::Minus => {
-                    trace!("Parsing infix subtraction");
-                    BinOpExprSymbol::with_precedence(Precedence::AddSub)
-                        .parse(self, left, token)
-                },
-                TokenType::Star => {
-                    trace!("Parsing with multiplication");
-                    BinOpExprSymbol::with_precedence(Precedence::MulDiv)
-                        .parse(self, left, token)
-                },
-                TokenType::Slash => {
-                    trace!("Parsing with division");
-                    BinOpExprSymbol::with_precedence(Precedence::MulDiv)
-                        .parse(self, left, token)
-                },
-                TokenType::Percent => {
-                    trace!("Parsing with modulo");
-                    BinOpExprSymbol::with_precedence(Precedence::Modulo)
-                        .parse(self, left, token)
-                },
-                TokenType::LeftParen => {
-                    trace!("Parsing function call");
-                    FnCallParser { }.parse(self, left, token)
-                },
-                TokenType::LeftAngle => {
-                    trace!("Parsing less than expression");
-                    BinOpExprSymbol::with_precedence(Precedence::EqualityCompare)
-                        .parse(self, left, token)
-                },
-                TokenType::RightAngle => {
-                    trace!("Parsing greater than expression");
-                    BinOpExprSymbol::with_precedence(Precedence::EqualityCompare)
-                        .parse(self, left, token)
-                },
-                TokenType::LessThanEquals => {
-                    trace!("Parsing lte expression");
-                    BinOpExprSymbol::with_precedence(Precedence::EqualityCompare)
-                        .parse(self, left, token)
-                },
-                TokenType::GreaterThanEquals => {
-                    trace!("Parsing gte expression");
-                    BinOpExprSymbol::with_precedence(Precedence::EqualityCompare)
-                        .parse(self, left, token)
-                },
-                TokenType::DoubleEquals => {
-                    trace!("Parsing equality expression");
-                    BinOpExprSymbol::with_precedence(Precedence::Equality)
-                        .parse(self, left, token)
-                },
-                TokenType::NotEquals => {
-                    trace!("Parsing not equals expression");
-                    BinOpExprSymbol::with_precedence(Precedence::Equality)
-                        .parse(self, left, token)
-                },
-                TokenType::PlusEquals
-                | TokenType::MinusEquals
-                | TokenType::StarEquals
-                | TokenType::PercentEquals
-                | TokenType::SlashEquals=> {
-                    trace!("Parsing assign op");
-                    AssignOpParser { }.parse(self, left, token)
-                },
+                Equals => AssignmentParser { }.parse(self, left, token),
+
+                Plus | Minus | Star | Slash | Percent =>
+                    BinOpExprSymbol { }.parse(self, left, token),
+
+                LeftParen => FnCallParser { }.parse(self, left, token),
+
+                LeftAngle | RightAngle =>
+                    BinOpExprSymbol { }.parse(self, left, token),
+
+                LessThanEquals | GreaterThanEquals =>
+                    BinOpExprSymbol { }.parse(self, left, token),
+
+                DoubleEquals | NotEquals =>
+                    BinOpExprSymbol { }.parse(self, left, token),
+
+                PlusEquals | MinusEquals | StarEquals | PercentEquals | SlashEquals =>
+                    AssignOpParser { }.parse(self, left, token),
+
                 _ => {
                     // If we can't match an infix then we need to parse the next
                     // expression.
@@ -361,32 +286,30 @@ impl<T: Tokenizer> Parser<T> {
 
     /// Parse a single statement.
     pub fn statement(&mut self) -> Result<Statement, ParseError> {
+        use self::TokenType::*;
+        trace!("Parsing statement for {:?}", self.next_type());
         match self.next_type() {
             // This may be refactorable with NLL
-            TokenType::Let => {
-                trace!("Parsing let statement");
+            Let => {
                 let token = self.consume();
                 DeclarationParser { }.parse(self, token)
             },
-            TokenType::Return => {
-                trace!("Parsing return statement");
+            Return => {
                 let token = self.consume();
                 ReturnParser { }.parse(self, token)
             },
-            TokenType::Do => {
-                trace!("Parsing do block");
+            Do => {
                 let token = self.consume();
                 DoBlockParser { }.parse(self, token)
             },
-            TokenType::If => {
-                trace!("Parsing an if block");
+            If => {
                 let token = self.consume();
                 IfBlockParser { }.parse(self, token)
             },
             _ => {
                 trace!("Using expr parser for statement");
                 self.expression(Precedence::Min)
-                    .map(Expression::to_statement)
+                    .map(Statement::Expression)
             }
         }
     }
@@ -484,65 +407,9 @@ impl<T: Tokenizer> Parser<T> {
 
     /// Create a new parser from the given tokenizer, initializing its fields to match
     pub fn new(tokenizer: T) -> Parser<T> {
-        use parse::symbol::*;
-        use lex::TokenType::*;
-        let expr_infix_map: HashMap<TokenType, Rc<InfixParser<Expression, T> + 'static>> =
-        hashmap![
-            Equals => Rc::new(AssignmentParser { }) as Rc<InfixParser<Expression, T>>,
-
-            Plus => BinOpExprSymbol::with_precedence(Precedence::AddSub),
-            Minus => BinOpExprSymbol::with_precedence(Precedence::AddSub),
-            Star => BinOpExprSymbol::with_precedence(Precedence::MulDiv),
-            Slash => BinOpExprSymbol::with_precedence(Precedence::MulDiv),
-
-            Percent => BinOpExprSymbol::with_precedence(Precedence::Modulo),
-
-            LeftParen => Rc::new(FnCallParser { }) as Rc<InfixParser<Expression, T>>,
-
-            LeftAngle => BinOpExprSymbol::with_precedence(Precedence::EqualityCompare),
-            RightAngle => BinOpExprSymbol::with_precedence(Precedence::EqualityCompare),
-            LessThanEquals => BinOpExprSymbol::with_precedence(Precedence::EqualityCompare),
-            GreaterThanEquals => BinOpExprSymbol::with_precedence(Precedence::EqualityCompare),
-
-            DoubleEquals => BinOpExprSymbol::with_precedence(Precedence::Equality),
-            NotEquals => BinOpExprSymbol::with_precedence(Precedence::Equality),
-
-            PlusEquals => Rc::new(AssignOpParser { }) as Rc<InfixParser<Expression, T>>,
-            MinusEquals => Rc::new(AssignOpParser { }) as Rc<InfixParser<Expression, T>>,
-            StarEquals => Rc::new(AssignOpParser { }) as Rc<InfixParser<Expression, T>>,
-            PercentEquals => Rc::new(AssignOpParser { }) as Rc<InfixParser<Expression, T>>,
-            SlashEquals => Rc::new(AssignOpParser { }) as Rc<InfixParser<Expression, T>>
-        ];
-        let expr_prefix_map: HashMap<TokenType, Rc<PrefixParser<Expression, T> + 'static>> =
-        hashmap![
-            If => Rc::new(IfExpressionParser { }) as Rc<PrefixParser<Expression, T>>,
-
-            Minus => UnaryOpExprSymbol::with_precedence(Precedence::NumericPrefix),
-            LeftParen => Rc::new(ParensParser { }) as Rc<PrefixParser<Expression, T>>,
-
-            Ident => Rc::new(IdentifierParser { }) as Rc<PrefixParser<Expression, T>>,
-            Literal => Rc::new(LiteralParser { }) as Rc<PrefixParser<Expression, T>>,
-        ];
-        let stmt_prefix_map: HashMap<TokenType, Rc<PrefixParser<Statement, T> + 'static>> =
-        hashmap![
-            Let => Rc::new(DeclarationParser { }) as Rc<PrefixParser<Statement, T>>,
-            Return => Rc::new(ReturnParser { }) as Rc<PrefixParser<Statement, T>>,
-            Do => Rc::new(DoBlockParser { }) as Rc<PrefixParser<Statement, T>>,
-            If => Rc::new(IfBlockParser { }) as Rc<PrefixParser<Statement, T>>,
-        ];
-        let item_prefix_map: HashMap<TokenType, Rc<PrefixParser<Item, T> + 'static>> =
-        hashmap![
-            Fn => Rc::new(FnDeclarationParser { }) as Rc<PrefixParser<Item, T>>,
-            Typedef => Rc::new(TypedefParser { }) as Rc<PrefixParser<Item, T>>,
-        ];
-
         Parser {
             tokenizer: tokenizer,
             lookahead: VecDeque::new(),
-            item_parsers: item_prefix_map,
-            stmt_prefix_parsers: stmt_prefix_map,
-            expr_prefix_parsers: expr_prefix_map,
-            expr_infix_parsers: expr_infix_map,
             indent_rules: Vec::new(),
         }
     }
@@ -562,12 +429,7 @@ impl<T: Tokenizer> Parser<T> {
 
     /// Get the current precedence
     fn current_precedence(&mut self) -> Precedence {
-        let looked_ahead = self.look_ahead(1).get_type();
- if let Some(infix_parser) = self.expr_infix_parsers.get(&looked_ahead).cloned() {
-            infix_parser.precedence()
-        } else {
-            Precedence::Min
-        }
+        Precedence::for_token(self.next_type(), false)
     }
 }
 
