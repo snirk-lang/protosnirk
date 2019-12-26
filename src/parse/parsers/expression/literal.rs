6ea8cba6294bf9868e65c0b29f15b3def69c8e81
S. B. Immington <snirk.immington@gmail.com>
2019-12-26 18:05:31 -0500

    Actually parse literals in LiteralParser

--- src/parse/parsers/expression/literal.rs
+++ src/parse/parsers/expression/literal.rs
@@ -14,12 +14,43 @@ use ast::*;
 /// ```
 pub struct LiteralParser { }
 impl<T: Tokenizer> PrefixParser<Expression, T> for LiteralParser {
-    fn parse(&self, _parser: &mut Parser<T>, token: Token) -> ParseResult<Expression> {
-        match *token.data() {
-            TokenData::NumberLiteral(val) =>
-                Ok(Expression::Literal(Literal::new(token, LiteralValue::Float(val)))),
-            TokenData::BoolLiteral(b) =>
-                Ok(Expression::Literal(Literal::new(token, LiteralValue::Bool(b)))),
+    fn parse(&self, _parser: &mut Parser<T>, token: Token)
+             -> ParseResult<Expression> {
+        match token.data() {
+            TokenData::NumberLiteral => {
+                match token.text().parse::<f64>() {
+                    Ok(val) =>
+                        Ok(Expression::Literal(
+                            Literal::new_float(token, val))),
+                    Err(_) =>
+                        // This is an internal error: tokenizer should've bailed
+                        Err(ParseError::ExpectedToken {
+                            expected: TokenType::Literal,
+                            got: token.get_type(),
+                            token: token
+                        })
+                }
+            },
+            TokenData::BoolLiteral => {
+                match token.text() {
+                    "true" =>
+                        Ok(Expression::Literal(
+                            Literal::new_bool(token, true))),
+                    "false" =>
+                        Ok(Expression::Literal(
+                            Literal::new_bool(token, false))),
+                    // This is an unexpected internal error.
+                    _ => Err(ParseError::ExpectedToken {
+                        expected: TokenType::Literal,
+                        got: token.get_type(),
+                        token: token
+                    })
+                }
+            },
+            TokenData::UnitLiteral => {
+                Ok(Expression::Literal(Literal::new_unit(token)))
+            },
+            // This is an unexpected internal error.
             _ => Err(ParseError::ExpectedToken {
                 expected: TokenType::Literal,
                 got: token.get_type(),
